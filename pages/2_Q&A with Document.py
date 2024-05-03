d1f2d76733d31b806dca77369167f6c88694a7f2
Vihanga Mihiranga <50793729+Vihanga22365@users.noreply.github.com>
2024-05-03 14:49:31 +0530

    Evaluation to same page

--- pages/2_Q&A with Document.py
+++ pages/2_Q&A with Document.py
@@ -20,6 +20,10 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
 from langsmith import Client
 import os
+from trulens_eval.feedback.provider import OpenAI
+from trulens_eval import Feedback
+import numpy as np
+from trulens_eval import TruChain, Tru
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
@@ -51,6 +55,50 @@ chain = (
     | model
     | StrOutputParser()
     )
+
+# Start Trulens
+# Start Trulens
+# Start Trulens
+
+tru=Tru()
+
+# Initialize provider class
+provider = OpenAI()
+
+# select context to be used in feedback. the location of context is app specific.
+from trulens_eval.app import App
+context = App.select_context(chain)
+
+from trulens_eval.feedback import Groundedness
+grounded = Groundedness(groundedness_provider=OpenAI())
+# Define a groundedness feedback function
+f_groundedness = (
+    Feedback(grounded.groundedness_measure_with_cot_reasons)
+    .on(context.collect()) # collect context chunks into a list
+    .on_output()
+    .aggregate(grounded.grounded_statements_aggregator)
+)
+
+# Question/answer relevance between overall question and answer.
+f_answer_relevance = (
+    Feedback(provider.relevance)
+    .on_input_output()
+)
+# Question/statement relevance between question and each context chunk.
+f_context_relevance = (
+    Feedback(provider.context_relevance_with_cot_reasons)
+    .on_input()
+    .on(context)
+    .aggregate(np.mean)
+)
+
+tru_recorder = TruChain(chain,
+    app_id='Chain1_ChatApplication',
+    feedbacks=[f_answer_relevance, f_context_relevance, f_groundedness])
+
+# End Trulens
+# End Trulens
+# End Trulens
 
 
 def get_response(question):
--- pages/3_Evaluation with Trulens.py
+++ pages/3_Evaluation with Trulens.py
@@ -12,7 +12,7 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
 from langchain_community.vectorstores import Chroma
 # import os
-import openaiz
+import openai
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import OllamaEmbeddings
